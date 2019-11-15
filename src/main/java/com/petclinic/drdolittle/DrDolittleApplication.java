4133391b051640f87005b36b10179b5dfddf2de1
serghalak <serghalak@gmail.com>
2019-11-15 14:52:17 +0200

    init project

---
+++ src/main/java/com/petclinic/drdolittle/DrDolittleApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic.drdolittle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrDolittleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrDolittleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petclinic/drdolittle/DrDolittleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic.drdolittle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrDolittleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
