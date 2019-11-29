ea6710b7271ccb36862c4caffd13c9de8deb59b5
serghalak <serghalak@gmail.com>
2019-11-29 18:14:47 +0200

    added spring data jpa service for vet closes #44

---
+++ pet-clinic-data/src/main/java/com/petclinic/drdolittle/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,45 @@
+package com.petclinic.drdolittle.services.springdatajpa;
+
+import com.petclinic.drdolittle.model.Pet;
+import com.petclinic.drdolittle.repositories.PetRepository;
+import com.petclinic.drdolittle.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return null;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Pet object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
